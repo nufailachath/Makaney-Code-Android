038427de3f64c1c02d2f4695b521bf2d4a5e8978
nufailachath <nufail.achath@zss.net>
2017-01-31 11:58:52 +0400

    Makaney Map/Layout

--- app/src/main/java/zss/net/makaneycode/MapsActivity.java
+++ app/src/main/java/zss/net/makaneycode/MapsActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -63,35 +64,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         EditText location_srch= (EditText) findViewById(R.id.makaney_search);
         String location = location_srch.getText().toString();
+
+        Geocoder geocoder = new Geocoder(this);
         List<Address> addressList = null;
-        if( location != null ||  !location.equals("") ){
-            Geocoder geocoder = new Geocoder(this);
-            try {
-               addressList = geocoder.getFromLocationName(location,1);
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-             Address address = addressList.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in Sydney"));
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        try {
+            addressList = geocoder.getFromLocationName(location,1);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else{
 
-            Toast.makeText(this, "We could not find"+location+"\r\n"+
-                   "Make sure your search is spelled correctly. Try adding a city, state, or zip code.", Toast.LENGTH_SHORT).show();
-        }
+
+        Address address = addressList.get(0);
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goToLocationZoom(lat,lng,12);
+
+        /*
+        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in Sydney"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+       */
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        goToLocation(36.5699702,-5.5012867);
 
         // Add a marker in Sydney and move the camera
        // LatLng sydney = new LatLng(-34, 151);
        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    //method for getting location  through latitude, longitude and zoom
+
+    public void goToLocation(double lat,double lng){
+        LatLng latLng = new LatLng(lat,lng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+    }
+    public void goToLocationZoom(double lat,double lng,float zoom){
+        LatLng latLng = new LatLng(lat,lng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,zoom);
+        mMap.animateCamera(cameraUpdate);
+
     }
 }
